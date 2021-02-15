128e0c3825288f3262ce05b23ec59bbfe06c3114
Nika Khachiashvili <o.sicknick@gmail.com>
2021-02-15 21:20:09 +0400

    user data will be saved when visiting user page

--- src/components/User Page/user-page.js
+++ src/components/User Page/user-page.js
@@ -1,5 +1,21 @@
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
 
 function UserPage(){
+
+    const userLogin = useParams().username;
+    
+    const [userData, setUserData] = useState([]);
+
+    useEffect(() => {
+        console.log('getting specific user data');
+        fetch(`https://api.github.com/users/${userLogin}`)
+        .then(response => response.json())
+        .then(data => setUserData(data))
+        .catch(error => {});
+    }, []);
+
     return (
         <div>
             User
